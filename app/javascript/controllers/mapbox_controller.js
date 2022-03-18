59975b5a2b10d87b93caa800e96f72b517300b43
Felix Dubois <felixdubois@hotmail.fr>
2022-03-18 16:47:28 +0100

    MAJ de geocode

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,6 +1,6 @@
-import { Controller } from "@hotwired/stimulus"
-import mapboxgl from "mapbox-gl"
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import { Controller } from "stimulus";
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 export default class extends Controller {
   static values = {
@@ -25,10 +25,18 @@ export default class extends Controller {
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
-      new mapboxgl.Marker()
+
+      const customMarker = document.createElement("div")
+      customMarker.className = "marker"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = "contain"
+      customMarker.style.width = "25px"
+      customMarker.style.height = "25px"
+
+      new mapboxgl.Marker(customMarker)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
-        .addTo(this.map)
+      .addTo(this.map)
     })
   }
   #fitMapToMarkers() {
